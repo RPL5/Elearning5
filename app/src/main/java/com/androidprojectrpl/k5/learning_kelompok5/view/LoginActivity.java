972b9dcbb1b26bc6a006bf317a773069a5b6ce19
DenisDarwa <saputradarwadenis@gmail.com>
2018-11-07 20:30:28 +0700

    edit text dan memperbaiki contrain layout

--- app/src/main/java/com/androidprojectrpl/k5/learning_kelompok5/view/LoginActivity.java
+++ app/src/main/java/com/androidprojectrpl/k5/learning_kelompok5/view/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.androidprojectrpl.k5.learning_kelompok5.R;
 
@@ -15,6 +16,18 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
     public void LOGIN(View view) {
+        EditText edUsername=findViewById(R.id.edittext1);
+        EditText edPassword=findViewById(R.id.edittext2);
+        String qUsername = edUsername.getText().toString();
+        String qPassword = edPassword.getText().toString();
+        if(qUsername.trim().equals("")) {
+            edUsername.setError("Silahkan mengisi username terlebih dahulu");
+            return;
+        }
+        if(qPassword.trim().equals("")) {
+            edPassword.setError("Silahkan mengisi Password terlebih dahulu");
+            return;
+        }
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(intent);
     }
