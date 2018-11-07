46c9ccb8d6ec24915d70fccb2feca92875ecf4a3
DenisDarwa <saputradarwadenis@gmail.com>
2018-11-07 22:31:51 +0700

    splash screen butuh e

--- app/src/main/java/com/androidprojectrpl/k5/learning_kelompok5/view/SplashActivity.java
+++ app/src/main/java/com/androidprojectrpl/k5/learning_kelompok5/view/SplashActivity.java
@@ -3,6 +3,8 @@ package com.androidprojectrpl.k5.learning_kelompok5.view;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
