5b381aa3641f5e6786b70710ed6dd02b3ae120bc
bayuirfan52 <bayuirfan52@gmail.com>
2018-11-08 11:33:00 +0700

    Fix fatal error R.class

--- app/src/main/java/com/androidprojectrpl/k5/learning_kelompok5/view/SplashActivity.java
+++ app/src/main/java/com/androidprojectrpl/k5/learning_kelompok5/view/SplashActivity.java
@@ -1,10 +1,8 @@
 package com.androidprojectrpl.k5.learning_kelompok5.view;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
