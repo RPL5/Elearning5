9901153f9363481b2d42e2b8af11967b83602a6c
bayuirfan52 <bayuirfan52@gmail.com>
2019-03-05 11:49:27 +0700

    Set Automatic enter Apps without Login

--- app/src/main/java/com/androidprojectrpl/k5/learning_kelompok5/view/SplashActivity.java
+++ app/src/main/java/com/androidprojectrpl/k5/learning_kelompok5/view/SplashActivity.java
@@ -1,17 +1,28 @@
 package com.androidprojectrpl.k5.learning_kelompok5.view;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.androidprojectrpl.k5.learning_kelompok5.view.login.LoginActivity;
+import com.androidprojectrpl.k5.learning_kelompok5.view.main.NavDrawer;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, LoginActivity.class);
+
+        SharedPreferences prefs = getSharedPreferences("SharedLearning",MODE_PRIVATE);
+
+        if (prefs.getBoolean("isLogin",false)){
+            intent = new Intent(this, NavDrawer.class);
+        }
+        else {
+            intent = new Intent(this, LoginActivity.class);
+        }
         startActivity(intent);
         finish();
     }
